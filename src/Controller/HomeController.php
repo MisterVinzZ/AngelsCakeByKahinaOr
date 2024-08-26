f858de1c45179432835203eb41e04f1ad46d2e9c
Vincent <vincentime77@live.fr>
2024-08-26 12:56:03 +0200

    boutique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produits = $produitRepository->findBy([], null, 6);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'produits' => $produits,
         ]);
     }
 }
